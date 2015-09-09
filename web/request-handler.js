83f8a77b87d1dd7b913e3b71dea2272294d5cfc4
jwareing <jwareing89@gmail.com>
2015-09-09 16:51:03 -0700

    Set up basic routing and added favicon

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,30 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+
+  if (req.url === '/'){
+    fs.readFile(archive.paths.index, function (err, data) {
+      if (err) {
+        throw err;
+      }
+      res.writeHeader(200, {"Content-Type": "text/html"});
+      res.end(data);
+    });
+  }
+  else if (req.url === '/favicon.ico'){
+    fs.readFile(archive.paths.favicon, function (err, data) {
+      if (err) {
+        throw err;
+      }
+      res.writeHeader(200);
+      res.end(data);
+    });
+  }
+  else {
+    res.writeHeader(404)
+    res.end('404, file not found');
+  }
 };
