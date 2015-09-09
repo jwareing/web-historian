b3e7f023e9469e9bba3d85b6713368f04b5f41ae
jwareing <jwareing89@gmail.com>
2015-09-09 16:51:18 -0700

    Implemented website download script

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,25 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+var http = require("http");
+var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+
+exports.download = function(url){
+  var request = http.request({host: url, port: 80}, function (res) {
+    var data = '';
+    res.on('data', function (chunk) {
+        data += chunk;
+    });
+    res.on('end', function () {
+      console.log(archive.paths.archivedSites + '/' + url);
+      fs.writeFile(archive.paths.archivedSites + '/' + url, data, function (err) {
+        if (err) throw err;
+        console.log('It\'s saved!');
+      });
+    });
+  });
+  request.on('error', function (e) {
+      console.log(e.message);
+  });
+  request.end();
+};
